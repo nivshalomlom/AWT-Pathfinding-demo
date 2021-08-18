ee0674873081103bdcafa32753b923fd17e7912a
Niv <nivshalomlom@gmail.com>
2021-08-18 04:03:52 +0300

    Constant module for less dependencies

---
+++ Constants/src/GridConstants.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+public class GridConstants {
+
+    // each tile's width and height
+    public final static int tileWidth = 50;
+    public final static int tileHeight = 50;
+
+    // enum of grid tile types
+    public enum TILE_TYPES {
+        WALL,
+        EMPTY,
+        VISITED,
+        PATH,
+        SOURCE,
+        DESTINATION
+    }
+
+    // the color of each tile type
+    public static Dictionary<TILE_TYPES, Color> tileColors = new Hashtable<>() {{
+        put(TILE_TYPES.WALL, Color.GRAY);
+        put(TILE_TYPES.VISITED, Color.ORANGE);
+        put(TILE_TYPES.PATH, Color.BLUE);
+        put(TILE_TYPES.SOURCE, Color.GREEN);
+        put(TILE_TYPES.DESTINATION, Color.RED);
+    }};
+
+}
